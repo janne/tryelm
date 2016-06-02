1fcf86ba4d2c1ff8e23fc15d73c788e0f9630c91
Jan Andersson <jan.andersson@gmail.com>
2016-06-02 23:16:06 +0200

    Add hello world Elm app

---
+++ src/Main.elm
@@ -0,0 +1,10 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+main =
+    div [ class "container" ]
+        [ h1 [] [ text "Hello, World!" ]
+        ]
