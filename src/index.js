d8cb260dcb2462a58bc854af2ffd74665cfdf70a
Jan Andersson <jan.andersson@gmail.com>
2016-06-03 00:37:54 +0200

    Compile Main.elm and output to console

--- src/index.js
+++ src/index.js
@@ -3,3 +3,8 @@
 require('./index.html');
 var Elm = require('./Main');
 var app = Elm.Main.embed(document.getElementById('main'));
+
+var compileToString = require("node-elm-compiler").compileToString;
+compileToString(["./Main.elm"], { yes: true }).then(function(data){
+    console.log("Text", data.toString());
+});
